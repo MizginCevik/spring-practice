f47691797c9e2ef9164d3d11f8d7e66e13d96d20
mizgin <mizgincvk@gmail.com>
2022-11-04 17:16:54 -0400

    Lab 6

---
+++ spring-06-orm/src/main/java/com/cydeo/Spring06OrmApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring06OrmApplication.class, args);
+    }
+
+}
