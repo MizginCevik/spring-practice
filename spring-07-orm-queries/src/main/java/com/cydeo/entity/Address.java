7c74e404dad97939fbd47dede3d9c4e992957e03
mizgin <mizgincvk@gmail.com>
2022-11-13 13:04:18 -0500

    Created tables

---
+++ spring-07-orm-queries/src/main/java/com/cydeo/entity/Address.java
@@ -0,0 +1,24 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String street;
+    private String zipCode;
+    @ManyToOne
+    private Customer customer;
+
+}
---
+++ spring-07-orm-queries/src/main/java/com/cydeo/enums/CartState.java
@@ -0,0 +1,7 @@
+package com.cydeo.enums;
+
+public enum CartState {
+
+    CREATED, SOLD
+
+}
