86f80a5966aa43ce37c630ce52c35f0491aded58
mizgin <mizgincvk@gmail.com>
2022-12-08 20:18:05 -0500

    API Lab

--- spring-08-rest/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
+++ spring-08-rest/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
@@ -1,10 +1,13 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.CustomerDTO;
 import com.cydeo.entity.Address;
+import com.cydeo.entity.Customer;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.AddressRepository;
 import com.cydeo.service.AddressService;
+import com.cydeo.service.CustomerService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +18,12 @@ public class AddressServiceImpl implements AddressService {
 
     private final AddressRepository addressRepository;
     private final MapperUtil mapperUtil;
+    private final CustomerService customerService;
 
-    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil) {
+    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil, CustomerService customerService) {
         this.addressRepository = addressRepository;
         this.mapperUtil = mapperUtil;
+        this.customerService = customerService;
     }
 
     @Override
@@ -36,7 +41,12 @@ public class AddressServiceImpl implements AddressService {
     @Override
     public AddressDTO update(AddressDTO addressDTO) {
         Address address = mapperUtil.convert(addressDTO, new Address());
+
+        CustomerDTO customer = customerService.findById(addressDTO.getCustomerId());
+        address.setCustomer(mapperUtil.convert(customer, new Customer()));
+
         addressRepository.save(address);
+
         return addressDTO;
     }
 
